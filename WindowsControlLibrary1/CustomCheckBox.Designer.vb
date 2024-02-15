af95ae559bb4316b313dffa937977a407d8ecc08
unknown <ProbablyXS>
2024-02-15 14:05:40 +0100

    v1.0

---
+++ WindowsControlLibrary1/CustomCheckBox.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CustomCheckBox
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 5
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(8, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(13, 13)
+        Me.Button1.TabIndex = 1
+        Me.Button1.TabStop = False
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.25!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(23, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "OFF"
+        '
+        'Button2
+        '
+        Me.Button2.CausesValidation = False
+        Me.Button2.Enabled = False
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(3, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(52, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.TabStop = False
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CustomCheckBox
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.DoubleBuffered = True
+        Me.Name = "CustomCheckBox"
+        Me.Size = New System.Drawing.Size(62, 35)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsControlLibrary1/CustomCheckBox.vb
@@ -0,0 +1,278 @@
+﻿'////////////////////////////////////////// BY VECR TEAM //////////////////////////////////////////
+
+'//////////////////////////////////////////              //////////////////////////////////////////
+
+Public Class CustomCheckBox
+
+    Dim speed As Integer = 4
+
+    Public Event Ovalclick()
+
+    Public Sub Ovalclick_Click() Handles Button1.Click
+
+        RaiseEvent Ovalclick()
+
+    End Sub
+
+    Dim ovalresultasPicturetrue As Image
+    Property A_ovalimagetrue() As Image
+
+        Get
+
+            A_ovalimagetrue = ovalresultasPicturetrue
+
+        End Get
+
+        Set(value As Image)
+
+
+            Button1.BackgroundImage = value
+            ovalresultasPicturetrue = value
+
+        End Set
+    End Property
+
+    Dim ovalresultasPicturefalse As Image
+
+    Public Property B_ovalimagefalse() As Image
+        Get
+
+            B_ovalimagefalse = ovalresultasPicturefalse
+
+        End Get
+
+        Set(value As Image)
+
+
+            Button1.BackgroundImage = value
+            ovalresultasPicturefalse = value
+
+        End Set
+    End Property
+
+    Dim resultasFalse As Color
+    Property ovalcolorFalse() As Color
+
+        Get
+
+            ovalcolorFalse = resultasFalse
+
+        End Get
+
+        Set(value As Color)
+
+
+            Button1.BackColor = value
+            resultasFalse = value
+
+        End Set
+
+    End Property
+
+    Dim resultasTrue As Color
+    Property ovalcolorTrue() As Color
+
+        Get
+
+            ovalcolorTrue = resultasTrue
+
+        End Get
+
+        Set(value As Color)
+
+            Button1.BackColor = value
+            resultasTrue = value
+
+        End Set
+    End Property
+
+    Public Property ovalLocation As Point
+
+        Get
+
+            Return Button1.Location
+
+        End Get
+
+        Set(value As Point)
+
+            Button1.Location = value
+
+        End Set
+    End Property
+
+
+    Dim RectangleresultasTrue As Color
+    Property A_RectangleovalcolorTrue() As Color
+
+        Get
+
+            A_RectangleovalcolorTrue = RectangleresultasTrue
+
+        End Get
+
+        Set(value As Color)
+
+            Button2.BackColor = value
+            RectangleresultasTrue = value
+
+        End Set
+    End Property
+
+
+    Dim RectangleresultasFalse As Color
+    Property B_RectangleovalcolorFalse() As Color
+
+        Get
+
+            B_RectangleovalcolorFalse = RectangleresultasFalse
+
+        End Get
+
+        Set(value As Color)
+
+            Button2.BackColor = value
+            RectangleresultasFalse = value
+
+        End Set
+    End Property
+
+    Dim RectangleborderresultasTrue As Color
+    Property RectangleborderovalcolorTrue() As Color
+
+        Get
+
+            RectangleborderovalcolorTrue = RectangleborderresultasTrue
+
+        End Get
+
+        Set(value As Color)
+
+            Button2.FlatAppearance.BorderColor = value
+            RectangleborderresultasTrue = value
+
+        End Set
+    End Property
+
+    Dim Ovalitsrespond As Boolean
+    Public Property Ovalchecked As Boolean
+
+        Get
+
+            Return Ovalitsrespond
+
+        End Get
+
+        Set(value As Boolean)
+
+            If (value = True) Then
+
+                Ovalitsrespond = True
+                Button1.Left = 36
+                Label1.Left = 10
+                Label1.Text = "ON"
+                'Button1.BackColor = resultasTrue
+                'Button1.BackgroundImage = ovalresultasPicturetrue
+                'RectangleShape1.BackColor = RectangleresultasTrue
+
+            ElseIf (value = False) Then
+
+                Ovalitsrespond = False
+                Button1.Left = 8
+                Label1.Left = 23
+                Label1.Text = "OFF"
+                'Button1.BackColor = resultasFalse
+                'Button1.BackgroundImage = ovalresultasPicturefalse
+                'RectangleShape1.BackColor = RectangleresultasFalse
+
+            End If
+
+        End Set
+    End Property
+
+    Private Sub OvalShape1(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If (Button1.Left = 8) Then 'gauche
+
+            Timer1.Start()
+            Ovalitsrespond = True
+            Label1.Visible = False
+
+        End If
+
+        If (Button1.Left = 36) Then 'droite
+
+            Timer2.Start()
+            Ovalitsrespond = False
+            Label1.Visible = False
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If Button1.Left <= 36 Then 'gauche
+
+            Button1.Left += speed
+
+        End If
+
+        If Button1.Left = 36 Then
+
+            'Button1.BackColor = resultasTrue
+            'Button1.BackgroundImage = ovalresultasPicturetrue
+            'RectangleShape1.BackColor = A_RectangleovalcolorTrue
+            'Ovalchecked = True
+            Label1.Left = 10
+            Label1.Visible = True
+            Label1.Text = "ON"
+            Timer1.Stop()
+
+        End If
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        If Button1.Left >= 8 Then 'droite
+
+            Button1.Left -= speed
+
+        End If
+
+        If Button1.Left = 8 Then
+
+            'Button1.BackColor = resultasFalse
+            'Button1.BackgroundImage = ovalresultasPicturefalse
+            'RectangleShape1.BackColor = B_RectangleovalcolorFalse
+            Ovalchecked = False
+            Label1.Left = 23
+            Label1.Visible = True
+            Label1.Text = "OFF"
+            Timer2.Stop()
+
+        End If
+
+    End Sub
+
+    Dim resultasLabelColor As Color
+    Property LabelColor() As Color
+
+        Get
+
+            LabelColor = resultasLabelColor
+
+        End Get
+
+        Set(value As Color)
+
+
+            Label1.ForeColor = value
+            resultasLabelColor = value
+
+        End Set
+
+    End Property
+
+End Class
